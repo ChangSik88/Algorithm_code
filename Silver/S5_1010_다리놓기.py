7a99042b25e98c41b7068a313811736e44e05428
ChangSik88 <abcdggh11@gmail.com>
2026-03-05 16:21:59 +0900

    fix:주석추가 및 파일 이름 변경

--- Silver/S5_1010_다리놓기.py
+++ Silver/S5_1010_다리놓기.py
@@ -8,6 +8,7 @@ if t:
     for i in range(t):
         n,m=map(int,sys.stdin.readline().split())
         tem=int((factorial(m))/(factorial(n)*factorial(m-n)))
+        #조합식 n!/r!(n-r)!
         anw.append(tem)
     while anw:
         print(anw.popleft())
